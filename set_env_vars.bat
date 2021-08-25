244c5332a1146290d1c90f358e037a92f18db82f
Jianwei Sun <jian58sun@gmail.com>
2021-08-24 22:45:52 -0500

    Setup CMake build environment

---
+++ set_env_vars.bat
@@ -0,0 +1,6 @@
+SET DEBUG=TRUE
+SET CL=/MP
+SET WINDOWS_SDK_INCLUDE_DIR=C:\Program Files (x86)\Windows Kits\10\Include\10.0.18362.0\um
+SET WINDOWS_SDK_LINK_DIR=C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x86
+SET Qt5_DIR=z:\lib\qt
+SET QT_QPA_PLATFORM_PLUGIN_PATH=z:\lib\qt\plugins\platforms
